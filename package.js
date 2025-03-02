c98496daaad8880a53c3c2d2229d9bbd89bc29dc
starsubject <doompi.webs@gmail.com>
2025-03-02 11:03:57 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "intranet-api",
+  "version": "1.0.0",
+  "description": "A simple intranet registry API",
+  "main": "network.js",
+  "scripts": {
+    "start": "vercel dev",
+    "deploy": "vercel deploy"
+  },
+  "dependencies": {}
+}
